e9a915d5eed1d52b2305caaa288dab1d66d26a02
dastan <dosyafree32@gmail.com>
2023-02-01 19:54:43 +0600

    it's task to classroom

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,11 +7,6 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        President pr = new President("Neil", 54);
-        President pr1 = new President("Nolly", 65);
-        President pr2 = new President("Cale", 45);
-        President pr3 = new President("Bink", 54);
-        President pr4 = new President("Nicol", 44);
         PresidentImpl pi = new PresidentImpl();
         System.out.println(pi.getById(5L));
     }
